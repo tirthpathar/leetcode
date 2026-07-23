c582bb417349fb651b5845b46c522b7bcdf67c02
tirthpathar <tirthpathar2007@gmail.com>
2026-07-24 00:48:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (47.62%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -5,38 +5,52 @@ public:
 
         stack<char> st;
 
-        for (int i = 0; i < s.length(); i++) {
-
-            if (s[i] != '#') {
+        for(int i = 0 ; i < s.size(); i++) {
+            if(s[i] != '#' ) {
                 st.push(s[i]);
+
             }
-            else {
-                if (!st.empty()) {
-                    st.pop();
-                }
-            }
+
+            else if(st.size() != 0) st.pop();
+            
         }
 
         return st;
     }
+
+    
 
     bool backspaceCompare(string s, string t) {
 
         stack<char> st1 = buildStack(s);
         stack<char> st2 = buildStack(t);
 
-        if (st1.size() != st2.size())
+        if(st1.size() != st2.size())
             return false;
 
-        while (!st1.empty()) {
+        while(st1.size() != 0) {
 
-            if (st1.top() != st2.top())
+            if(st1.size() != st2.size()) return false;
+            
+
+            if(st1.top() != st2.top()) {
                 return false;
 
+
+            }
+
+            
             st1.pop();
             st2.pop();
+
         }
 
         return true;
+
+
+
+        
+
+
     }
 };
